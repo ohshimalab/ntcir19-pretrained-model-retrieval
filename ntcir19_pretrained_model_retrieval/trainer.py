155a86160cd5ed830605c1ebe350fa003c7bf3b4
longpham28 <huulongpham28@gmail.com>
2026-02-22 22:39:28 +0900

    fix: fix save model checkpont numbers

--- ntcir19_pretrained_model_retrieval/trainer.py
+++ ntcir19_pretrained_model_retrieval/trainer.py
@@ -288,7 +288,7 @@ def run_experiment(
         load_best_model_at_end=True,
         metric_for_best_model=TRAINING_CONFIG["metric_for_best_model"],
         greater_is_better=False,
-        save_total_limit=2,
+        save_total_limit=1,
         report_to="none",
         seed=seed,
         data_seed=seed,
